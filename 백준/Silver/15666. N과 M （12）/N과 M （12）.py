a8978fa3b9aab9816fc3f2288778c3c1880f5f37
이호준 <akk808@naver.com>
2023-08-29 23:14:18 +0900

    [Silver II] Title: N과 M (12), Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+nums = list(set(map(int, input().split())))
+nums.sort()
+answer = ['0'] + ([''] * m)
+
+def backtrack(k):
+    if k == m+1:
+        print(' '.join(answer[1:]))
+        return
+
+    for i in range(len(nums)):
+        if nums[i] >= int(answer[k-1]):
+            answer[k] = str(nums[i])
+            backtrack(k+1)
+
+backtrack(1)
