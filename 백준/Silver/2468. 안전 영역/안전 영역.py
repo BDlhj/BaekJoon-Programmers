f9bff6f326e780b30d7844999b5f7a9b1e2d27a7
이호준 <akk808@snu.ac.kr>
2024-03-13 14:19:01 +0900

    [Silver I] Title: 안전 영역, Time: 756 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -2,34 +2,43 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def bfs(row, col):
-    q = deque([(row, col)])
+dr = [-1,0,1,0]
+dc = [0,1,0,-1]
 
-    while q:
-        cy, cx = q.popleft()
+def bfs(height):
+    while qu:
+        r,c = qu.popleft()
         for i in range(4):
-            ny = cy + dy[i]
-            nx = cx + dx[i]
-            if (0 <= ny < n and 0 <= nx < n) and not visited[ny][nx] and graph[ny][nx] > height:
-                visited[ny][nx] = True
-                q.append((ny, nx))
+            row = r+dr[i]
+            col = c+dc[i]
+            if 0 <= row < N and 0 <= col < N:
+                if not visited[row][col] and arr[row][col] > height:
+                    visited[row][col] = True
+                    qu.append((row,col))
 
-n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-dy = [0, 1, 0, -1]
-dx = [1, 0, -1, 0]
-answer = 1
+N = int(input().strip())
+arr = []
+qu = deque()
+max_n = 0
+max_cnt = 0
+for _ in range(N):
+    row = list(map(int,input().strip().split()))
+    max_row = max(row)
+    if max_n < max_row:
+        max_n = max_row
+    arr.append(row)
 
-for height in range(1, 100):
-    visited = [[False] * n for _ in range(n)]
-    num_of_safe_area = 0
-    for row in range(n):
-        for col in range(n):
-            if graph[row][col] > height and not visited[row][col]:
-                visited[row][col] = True
-                bfs(row, col)
-                num_of_safe_area += 1
-    
-    answer = max(answer, num_of_safe_area)
+for i in range(0,max_n):
+    cnt = 0
+    visited = [[False] * N for _ in range(N)]
+    for r in range(N):
+        for c in range(N):
+            if not visited[r][c] and arr[r][c] > i:
+                visited[r][c] = True
+                qu.append((r,c))
+                bfs(i)
+                cnt += 1
+    if max_cnt < cnt:
+        max_cnt = cnt
 
-print(answer)
+print(max_cnt)
